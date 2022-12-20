6e9de5a8266d895b1c0a8149a8ca979f7c7055a9
Tae-kyun Kim <ktk03115@gmail.com>
2022-12-20 18:54:00 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,15 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(displayWidth / 2, displayHeight / 2);
+  createCanvas(displayWidth / 2.5, displayWidth / 3);
+  noStroke();
+  fill("#23A41A");
+
 }
 
 function draw() {
   background(220);
-  let margin = 100;
-  image(tree, margin, margin/2, (height-margin)/3*2, (height-margin));
+  let margin = 25;
+  rect(0, height*0.625, width, height*0.375);
+  image(tree, margin, margin/2, (height*0.75-margin)/3*2, (height*0.75-margin));
 }
