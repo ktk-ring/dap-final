eb0a54370bd5e33826de610f35921ee73d8ba9ca
Tae-kyun Kim <ktk03115@gmail.com>
2022-12-20 23:12:31 +0900

    Objectify persimmon shapes

--- persimmon.js
+++ persimmon.js
@@ -1,24 +1,19 @@
-class Persimmon {
-  constructor(x, y, percent) {
-    this.x = x
-    this.y = y;
-    this.w = 100 / 3;
-    this.p = percent;
+class Fruits {
+  constructor(x, y, w) {
+    this.pos = createVector(x,y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.w = w;
   }
 
-  fruit(canvasName) {
-    canvasName.push();
-    canvasName.noStroke();
-    canvasName.fill(255, 165, 0);
-    if (random() < this.p && this.y < 275) {
-      canvasName.circle(this.x, this.y, this.w * 0.75);
-    }
-    this.x += this.w;
-    if (this.x > 325) {
-      this.y += this.w;
-      this.x = 50;
-    }
-    canvasName.pop();
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
 
+  show(){
+    fill(255,165,0);
+    circle(this.pos.x, this.pos.y, this.w);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,35 +1,54 @@
 let tree;
 let fruitC;
 
-let persimmon = new Persimmon(50, 50, 0.5);
+let x, y, w;
+
+let count = 10;
+
+let persimmon = [];
 
 function preload() {
   tree = loadImage("data/tree.png");
 }
 
 function setup() {
-  w = displayWidth / 2.5;
-  h = displayWidth / 3;
-  createCanvas(w, h);
-  fruitCanvas = createGraphics(w, h);
+  createCanvas(displayWidth / 2.5, displayWidth / 3);
+  fruitCanvas = createGraphics(displayWidth / 2.5, displayWidth / 3);
   noStroke();
+
+  x = 50;
+  y = 50;
+  w = 100 / 3;
 }
 
 function draw() {
   background(220);
+
   let margin = 25;
   fill("#23A41A");
   rect(0, height * 0.625, width, height * 0.375);
   image(tree, margin, margin / 2, (height * 0.75 - margin) / 3 * 2, (height * 0.75 - margin));
 
   image(fruitCanvas, 0, 0);
-  
+
   push();
   strokeWeight(10);
   stroke(0);
   line(mouseX, mouseY, mouseX - 10, mouseY - 100);
   pop();
 
-  persimmon.fruit(fruitCanvas);
+  for (; y < 275; y += w) {
+    for (; x <= 325; x += w) {
+      if (random() < 0.5) {
+        fill(255, 165, 0);
+        persimmon.push(new Fruits(x, y, w));
+      }
+    }
+    x = 50;
+  }
 
+  for (fruit of persimmon) {
+    fruit.update();
+    fruit.show();
+  }
 }
