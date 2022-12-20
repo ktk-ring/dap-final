1a394c3902ced0272618409ee883d3edabdc2b75
Tae-kyun Kim <ktk03115@gmail.com>
2022-12-20 17:17:05 +0900

    Add tree.png

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+let tree;
+
+function preload() {
+  tree = loadImage("data/tree.png");
+}
+
 function setup() {
-  createCanvas(displayWidth, displayHeight-300);
+  createCanvas(displayWidth / 2, displayHeight / 2);
 }
 
 function draw() {
   background(220);
+  let margin = 100;
+  image(tree, margin, margin/2, (height-margin)/3*2, (height-margin));
 }
